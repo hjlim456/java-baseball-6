9fb8d4e9134c33eb927916d2b69c8cc6ef692355
hyojun <hjlim45678@gmail.com>
2024-09-04 09:16:25 +0900

    feat : 사용자가 입력한 숫자 읽어오는 기능 구현

--- src/main/java/baseball/MainController.java
+++ src/main/java/baseball/MainController.java
@@ -15,6 +15,7 @@ public class MainController {
             //사용자 입력받기
 
         Computer computer = new Computer(createRandomNumber());
+        User user = new User(readUserNumbers());
         //게임 시작
             //컴퓨터 숫자와 사용자 숫자 비교
         //게임 종료
@@ -22,7 +23,18 @@ public class MainController {
             // 재시작 여부 묻고 재시작 or 종료
     }
 
+    private static Numbers readUserNumbers() {
+        System.out.println("숫자 야구 게임을 시작합니다.\n숫자를 입력해주세요 : ");
+        String userInput = Console.readLine();
 
+        List<Integer> userNumbers = new ArrayList<>();
+
+        for (int i = 0; i < userInput.length(); i++) {
+            int number = Integer.parseInt(String.valueOf(userInput.charAt(i)));
+            userNumbers.add(number);
+        }
+        return new Numbers(userNumbers);
+    }
 
     private static Numbers createRandomNumber() {
         List<Integer> numbers = new ArrayList<>();
